ba732c9664d98f0245a83712566383c9fd44adda
sapetnioc <sapetnioc@users.noreply.github.com>
2025-04-29 12:13:03 +0200

    Created blank site with Jenkyll

--- jekyll_activation.sh
+++ jekyll_activation.sh
@@ -6,7 +6,3 @@ fi
 if [ ! -e "$CONDA_PREFIX/share/rubygems/bin/bundler" ]; then
     gem install bundler jekyll
 fi
-
-# if [ ! -e "$PIXI_PROJECT_ROOT/site" ]; then
-#     jekyll new "$PIXI_PROJECT_ROOT" --blank
-# fi
